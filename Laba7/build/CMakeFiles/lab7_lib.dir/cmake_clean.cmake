4a7c14435460064d3a0b9b6e88efe531c658ed89
DaniilTsybulko <113765458+DaniilTsybulko@users.noreply.github.com>
2023-12-28 21:25:28 +0300

    Add files via upload

---
+++ Laba7/build/CMakeFiles/lab7_lib.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab7_lib.dir/main.cpp.o"
+  "CMakeFiles/lab7_lib.dir/main.cpp.o.d"
+  "CMakeFiles/lab7_lib.dir/src/errant_knight.cpp.o"
+  "CMakeFiles/lab7_lib.dir/src/errant_knight.cpp.o.d"
+  "CMakeFiles/lab7_lib.dir/src/npc.cpp.o"
+  "CMakeFiles/lab7_lib.dir/src/npc.cpp.o.d"
+  "CMakeFiles/lab7_lib.dir/src/slaver.cpp.o"
+  "CMakeFiles/lab7_lib.dir/src/slaver.cpp.o.d"
+  "CMakeFiles/lab7_lib.dir/src/squirrel.cpp.o"
+  "CMakeFiles/lab7_lib.dir/src/squirrel.cpp.o.d"
+  "liblab7_lib.a"
+  "liblab7_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab7_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Laba7/build/CMakeFiles/lab7_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab7_test.dir/test/lab7_test.cpp.o"
+  "CMakeFiles/lab7_test.dir/test/lab7_test.cpp.o.d"
+  "lab7_test"
+  "lab7_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab7_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
